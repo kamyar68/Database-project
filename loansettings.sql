2d6111552ffbef77fa55bc510780721e7b14584f
Anna Gorodetskaya <syrbysa@gmail.com>
2014-03-09 22:05:16 +0400

    date change

--- loansettings.sql
+++ loansettings.sql
@@ -12,7 +12,7 @@ for each row
 WHEN (SELECT available from items where items.IID=NEW.IID)
 begin
 update loans set Sdate=(SELECT date('now')) where CID=NEW.CID and IID=NEW.IID;
-update loans set Ddate= NEW.Sdate+(select loanduration from items,loans where items.IID=NEW.IID) where CID=NEW.CID and IID=NEW.IID;
+update loans set Ddate= select date(NEW.Sdate, '+(select loanduration from items,loans where items.IID=NEW.IID) where CID=NEW.CID and IID=NEW.IID day');
 update loans set loanID=((Select count (*) from loans)+1) where CID=NEW.CID and IID=NEW.IID;
 update items set available= FALSE where items.IID=NEW.IID;
 end;
