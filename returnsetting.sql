5e2b2e80cebc13ce512f66416622707070276e5c
Anna Gorodetskaya <syrbysa@gmail.com>
2014-03-28 19:42:04 +0400

    Edit2

--- returnsetting.sql
+++ returnsetting.sql
@@ -2,8 +2,9 @@ create trigger ret1
 after insert on returning
 for each row
 begin
-update ret set retdate=(SELECT date('now')) where loanID=NEW.loanID;
-update items set available=1 where IID= (select IID from loans where loans.loanID=NEW.loandID);
+update returning set retdate=(SELECT date('now')) where loanID=NEW.loanID;
+update items set available=1 where IID= (select IID from loans where loans.loanID=NEW.loanID);
+update loans set returned=1 where loans.loanID=NEW.loanID;
 end;
 
 
@@ -14,7 +15,7 @@ for each row
 when(abs((select julianday('now')) - (select julianday(Sdate) from loans where loanID=NEW.loanID))> (select loanduration from items where IID=(select IID from loans where loanID=NEW.loanID)))
 
 begin
-insert into fee values('Reservation',NEW.loanID,1*(abs((select julianday('now')) - (select julianday(Sdate) from loans where loans.loandID=NEW.loanID)) - (select loanduration from items where IID= (select IID from loans where loans.loanID=NEW.loanID))),'Pending');
+insert into fee values('Delayed loan',NEW.loanID,1*(abs((select julianday('now')) - (select julianday(Sdate) from loans where loans.loanID=NEW.loanID)) - (select loanduration from items where IID= (select IID from loans where loans.loanID=NEW.loanID))),'pending');
 update loans set returned=1 where loans.loanID=NEW.loanID;
 end;
 
